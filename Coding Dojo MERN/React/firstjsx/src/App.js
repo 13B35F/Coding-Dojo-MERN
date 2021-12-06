b67ddef46ab8e25e8cc9fb2b4911ff945b7795fd
Yanghoon <yanghoon.ji@gmail.com>
2021-12-05 19:39:05 -0500

    First JSX

---
+++ Coding Dojo MERN/React/firstjsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className="header">Hello Dojo!</h1>
+    <p className="things">Things I need to do:</p>
+    <ul className="list">
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+    </>
+  );
+}
+
+export default App;
