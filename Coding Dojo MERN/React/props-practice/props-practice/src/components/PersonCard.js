eda2de4a08ade21cb6fe05bc6b9bc1886f57f8e2
Yanghoon <yanghoon.ji@gmail.com>
2021-12-12 17:28:35 -0500

    Prop it modified

--- Coding Dojo MERN/React/props-practice/props-practice/src/components/PersonCard.js
+++ Coding Dojo MERN/React/props-practice/props-practice/src/components/PersonCard.js
@@ -1,27 +1,19 @@
 import React, { useState } from 'react'
-
-
-
-
 const PersonCard = (props) => {
 
     const {firstName, lastName, age, hairColor} = props;
-    const [currentAge, setCurrentAge] = useState(props.age);
+    const [currentAge, setCurrentAge] = useState(age);
     const addAge = ()=> {
-        setCurrentAge(currentAge + "1")
+        setCurrentAge((pAge) => pAge + 1)
     }
     return(
-        
-    
         <div>
             <p>{lastName}, {firstName}</p>
             <p>{currentAge}</p>
-            {/* <button onClick={()=>{ setCurrentAge(setCurrentAge + 1)}}>Birthday</button> */}
-            <button onClick={()=>{setCurrentAge(currentAge + 1)}}> Birthday Button for {firstName} {lastName} </button> 
+            <button onClick={ addAge }> Birthday Button for {firstName} {lastName} </button> 
             <p>Hair Color: {hairColor}</p>
         </div>
     )
 }
-
 
 export default PersonCard;
